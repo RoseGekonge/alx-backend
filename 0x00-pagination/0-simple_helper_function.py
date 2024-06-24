0f77da65c80f347c35f77ca71767197e35e9f615
RoseGekonge <rgekonge2001@gmail.com>
2024-06-24 17:47:50 +0300

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,6 +5,16 @@ pagging
 
 
 def index_range(page, page_size):
+    """
+    find the index ranges.
+
+    Args:
+        page: current page number
+        page_size: data per page
+
+    Return:
+        tuple of the ranges.
+    """
     start_index = (page - 1) * page_size
     end_index = page * page_size
 
