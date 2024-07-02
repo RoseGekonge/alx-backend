455d0592ede591f6ad4b87e38fcd1d969f413d80
RoseGekonge <rgekonge2001@gmail.com>
2024-07-02 19:32:50 +0300

    learning flask

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -9,18 +9,20 @@ from flask_babel import Babel
 
 class Config(object):
     """
-    Application configuration class
+    Application configuration class for language and timezone
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-# Instantiate the application object
+# Creating a new flask app
 app = Flask(__name__)
+
+# This is meant to apply the configaration to the flask app
 app.config.from_object(Config)
 
-# Wrap the application with Babel
+# Instantiate the application object and storing it in a module-level variable named babel
 babel = Babel(app)
 
 
